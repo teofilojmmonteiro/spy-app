df0f955b119cfb570f3f1eddd5997653ff700670
Teofilo Monteiro <te0ofilo@gmail.com>
2014-07-02 23:25:09 +0100

    Added empty class CodeString

---
+++ Spy APP/CodeString.h
@@ -0,0 +1,13 @@
+//
+//  CodeString.h
+//  Spy APP
+//
+//  Created by Teófilo on 02/07/14.
+//  Copyright (c) 2014 TeoProductions. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CodeString : NSObject
+
+@end
